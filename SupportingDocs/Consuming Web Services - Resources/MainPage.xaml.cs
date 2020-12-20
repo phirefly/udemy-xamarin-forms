3e33022bc1b68b96ba4561e61c9855debfe748d2
Raphael Villas <raphael.villas@gmail.com>
2020-12-20 11:52:20 -0500

    Added more Supporting Docs

---
+++ SupportingDocs/Consuming Web Services - Resources/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public class Post 
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Body { get; set; }
+	}
+
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+		}
+
+		void OnAdd(object sender, System.EventArgs e)
+		{
+		}
+
+		void OnUpdate(object sender, System.EventArgs e)
+		{
+		}
+
+		void OnDelete(object sender, System.EventArgs e)
+		{
+		}
+	}
+}
---
+++ SupportingDocs/Using SQLite - Resources/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public class Recipe 
+	{
+		public int Id { get; set; }
+		public string Name  { get; set; }
+	}
+
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void OnAdd(object sender, System.EventArgs e)
+		{
+		}
+
+		void OnUpdate(object sender, System.EventArgs e)
+		{
+		}
+
+		void OnDelete(object sender, System.EventArgs e)
+		{
+		}
+	}
+}
